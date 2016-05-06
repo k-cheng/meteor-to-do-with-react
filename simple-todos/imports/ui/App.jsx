5b9f3641033bb0aa7c73eddaf65015c41d260d86
Kevin Cheng <09chengk@gmail.com>
2016-05-05 22:55:27 -0700

    sets up task collection with mongodb

--- simple-todos/imports/ui/App.jsx
+++ simple-todos/imports/ui/App.jsx
@@ -1,17 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Tasks } from '../api/tasks.js';
 import Task from './Task.jsx';
 
-export default class App extends Component {
-  getTasks() {
-    return [
-    { _id: 1, text: 'Task 1' },
-    { _id: 2, text: 'Task 2' },
-    { _id: 3, text: 'Task 3' }
-    ];
-  };
-
+// app component
+class App extends Component {
   renderTasks() {
-    return this.getTasks().map((task) => (
+    return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
       ));
   }
@@ -30,3 +25,15 @@ export default class App extends Component {
     );
   }
 }
+
+App.propTypes = {
+  tasks: PropTypes.array.isRequired,
+};
+
+// 'createContainer wrapper is needed to use react-meteor-data'
+export default createContainer(() => {
+  return {
+    tasks: Tasks.find({}).fetch(),
+  };
+}, App);
+
--- simple-todos/server/main.js
+++ simple-todos/server/main.js
@@ -1,5 +1,3 @@
-import { Meteor } from 'meteor/meteor';
-
-Meteor.startup(() => {
-  // code to run on server at startup
-});
+// need to import on the server to create MongoDB collection
+// and sets up the routes to get the data to the client
+import '../imports/api/tasks.js';
