36d00422f68e8f09599d7faba9d908ca7819f4c2
Kevin Cheng <09chengk@gmail.com>
2016-05-05 23:57:36 -0700

    can input custom tasks

--- simple-todos/imports/ui/App.jsx
+++ simple-todos/imports/ui/App.jsx
@@ -1,10 +1,23 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Tasks } from '../api/tasks.js';
 import Task from './Task.jsx';
 
 // app component
 class App extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // get the text field from the React ref
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
   renderTasks() {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
@@ -16,6 +29,13 @@ class App extends Component {
       <div className='container'>
         <header>
           <h1>Todo List</h1>
+          <form className='new-task' onSubmit={this.handleSubmit.bind(this)} >
+            <input
+              type='text'
+              ref='textInput'
+              placeholder='Add new tasks!'
+            />
+          </form>
         </header>
 
         <ul>
@@ -33,7 +53,7 @@ App.propTypes = {
 // 'createContainer wrapper is needed to use react-meteor-data'
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 }, App);
 
